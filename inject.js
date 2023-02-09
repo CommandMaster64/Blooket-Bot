88c665ffd15fbaaec55c18c51038165abc48e6cd
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2023-02-08 16:29:19 -0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,6 +1,3 @@
-let script = document.createElement("script");
-fetch("https://raw.githubusercontent.com/CommandMaster64/Blooket-Bot/main/Blooketbot.js").then(response => response.text()).then(data => {
-    script.innerHTML = data.replaceAll("\n", "").replaceAll("  ", " ");
-});
-document.body.appendChild(script);
-setTimeout(() => { thing(); }, 20);
+let script = document.createElement("script"); fetch("https://raw.githubusercontent.com/CommandMaster64/Blooket-Bot/main/Blooketbot.js").then(response => response.text()).then(data => { script.innerHTML = data.replaceAll("\n", "").replaceAll("  ", " "); }); document.body.appendChild(script); setTimeout(() => { thing(); }, 150);
+// javascript: req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/CommandMaster64/Blooket-Bot/main/inject.js'); req.onload = function() { eval(this.responseText + 'snake.more_menu();'); }; req.send();
+// The requests are not my code ^^^^^ Paste in bookmark
